79536dee4fb4b82a6341c3bce27a356ca9364133
piggy-devil <muupaza@gmail.com>
2024-07-18 19:54:49 +0700

    CardWrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { LoginButton } from "@/components/auth/LoginButton";
+import { LoginButton } from "@/app/auth/_components/LoginButton";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
