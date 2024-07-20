6a4103e4f7abf94753b4dbfca4b0bdd2b4111f71
piggy-devil <muupaza@gmail.com>
2024-07-20 15:05:05 +0700

    Create User

--- actions/auth/register.ts
+++ actions/auth/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "../users/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid credentials!" };
   }
 
-  return { success: "Register Success!" };
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
