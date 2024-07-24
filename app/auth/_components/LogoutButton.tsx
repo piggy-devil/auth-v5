8db447e978b141a6e923b6e420b84bc4eb0069b7
piggy-devil <muupaza@gmail.com>
2024-07-24 16:28:00 +0700

    Add Image Avatar

--- app/auth/_components/LogoutButton.tsx
+++ app/auth/_components/LogoutButton.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import { logout } from "@/actions/auth/logout";
+import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
 interface LoginButtonProps {
   children?: ReactNode;
+  className?: string;
 }
 
-export const LogoutButton = ({ children }: LoginButtonProps) => {
+export const LogoutButton = ({ children, className }: LoginButtonProps) => {
   const onClick = () => {
     logout();
   };
 
   return (
-    <span onClick={onClick} className="cursor-pointer">
+    <span onClick={onClick} className={cn("cursor-pointer", className)}>
       {children}
     </span>
   );
--- app/auth/_components/UserButton.tsx
+++ app/auth/_components/UserButton.tsx
@@ -4,34 +4,64 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useCurrentUser } from "@/hooks/useCurrentUser";
 import { FaUser } from "react-icons/fa";
 import { LogoutButton } from "./LogoutButton";
-import { LogOut } from "lucide-react";
+import { LogOut, Settings, User } from "lucide-react";
+import Link from "next/link";
 
 export const UserButton = () => {
   const user = useCurrentUser();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
+      <DropdownMenuTrigger className="focus:outline-none active:outline-none">
+        <Avatar className="bg-red-500">
           <AvatarImage src={user?.image} />
           <AvatarFallback className="bg-sky-500">
             <FaUser className="text-white" />
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-40">
-        <LogoutButton>
+      <DropdownMenuContent className="w-48" align="end">
+        <DropdownMenuGroup>
           <DropdownMenuItem>
-            <LogOut className="h-4 w-4 mr-2" />
-            Logout
+            <Avatar className="mr-2">
+              <AvatarImage src={user?.image} />
+              <AvatarFallback className="bg-sky-500">
+                <FaUser className="text-white" />
+              </AvatarFallback>
+            </Avatar>
+            <span>{user?.name || "My Account"}</span>
           </DropdownMenuItem>
-        </LogoutButton>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Link href="/profile" className="flex flex-row">
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/settings" className="flex flex-row">
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogoutButton className="flex flex-row">
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Log out</span>
+          </LogoutButton>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
